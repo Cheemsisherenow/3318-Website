c31ab41db3c3a58226cb130f1630030a182fd84e
Zihong <Zihongchen0227@gmail.com>
2026-02-28 06:52:53 -0500

    Fix Vite base path for GitHub Pages

--- vite.config.js
+++ vite.config.js
@@ -5,5 +5,5 @@ import tailwindcss from '@tailwindcss/vite'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss()],
-  base: "/3318-Website"
+  base: "/3318-Website/"
 })
